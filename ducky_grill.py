ffcc077a86ed969157e27663a24872c8ead42491
George Sedky <george.m.sedky@gmail.com>
2016-09-16 17:50:44 +0200

    Add detect only HID devices

--- ducky_grill.py
+++ ducky_grill.py
@@ -4,6 +4,26 @@ from threading import *
 
 printLock = Semaphore(value = 1)
 
+class find_class(object):
+    def __init__(self, class_):
+        self._class = class_
+    def __call__(self, device):
+        # first, let's check the device
+        if device.bDeviceClass == self._class:
+            return True
+        # ok, transverse all devices to find an
+        # interface that matches our class
+        for cfg in device:
+            # find_descriptor: what's it?
+            intf = usb.util.find_descriptor(
+                                        cfg,
+                                        bInterfaceClass=self._class
+                                )
+            if intf is not None:
+                return True
+
+        return False
+
 def getId(dev):
     return (dev.idVendor, dev.idProduct, dev.bus, dev.address)
 
@@ -12,15 +32,16 @@ def handle_new_device(dev):
         printLock.acquire()
         print "new device inserted"
         print getId(dev)
+        print dev
     finally:
         printLock.release()
 
 def detect_hot_plug():
-    devices =  list(usb.core.find(find_all=True))
+    devices =  list(usb.core.find(find_all=True, custom_match=find_class(3)))
     devices_set = set( getId(dev) for dev in devices )
     while True:
         try:
-            dev_new = list(usb.core.find(find_all=True))
+            dev_new = list(usb.core.find(find_all=True, custom_match=find_class(3)))
             dev_new_set = set( getId(dev) for dev in dev_new )
             new_dev_id = set(dev_new_set) - set(devices_set)
             if len(new_dev_id) > 0 :
